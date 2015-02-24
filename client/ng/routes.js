c18327d8c716a12315e86b8372d267c97ff0bb19
Kevin Lee <kglee79@gmail.com>
2015-02-24 12:15:08 -0800

    Improve style and layout of dashboard.

--- client/ng/routes.js
+++ client/ng/routes.js
@@ -1,29 +1,29 @@
-angular.module('dashboardApp').config(function ($routeProvider, $locationProvider) {
+angular.module('dashboardApp').config(function ($routeProvider) {
         //$locationProvider.html5Mode(true);
         $routeProvider.
             when('/', {
-                templateUrl: 'views/charts/charts.html',
+                templateUrl: 'views/tabs/overview.html',
+                controller: 'navCtrl'
+            }).
+            when('/overview', {
+                templateUrl: 'views/tabs/overview.html',
+                controller: 'navCtrl'
+            }).
+            when('/meeting', {
+                templateUrl: 'views/tabs/meeting.html',
+                controller: 'navCtrl'
+            }).
+            when('/email', {
+                templateUrl: 'views/tabs/email.html',
+                controller: 'navCtrl'
+            }).
+            when('/morale', {
+                templateUrl: 'views/tabs/morale.html',
                 controller: 'navCtrl'
             }).
             otherwise({
                 redirectTo: '/'
             });
-        //    .when('/register', {
-        //    controller: 'RegisterCtrl',
-        //    templateUrl: 'views/register.html'
-        //}).when('/posts', {
-        //    controller: 'PostsCtrl',
-        //    templateUrl: 'views/register.html'
-        //}).when('/meetingareas', {
-        //    controller: 'MeetingAreasCtrl',
-        //    templateUrl: 'views/meetingAreas.html'
-        //}).when('/logout', {
-        //    controller: 'LogoutCtrl',
-        //    templateUrl: 'views/logout.html'
-        //}).when('/login', {
-        //    controller: 'LoginCtrl',
-        //    templateUrl: 'views/login.html'
-        //})
     }
 )
 
